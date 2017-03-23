b36b5a35b4cf14fdc1bc390347a5cc9e30d5509f
Chase-UCF <chaserobison8@gmail.com>
2017-03-23 15:07:33 -0400

    work

---
+++ assets/javascript/app.js
@@ -0,0 +1,52 @@
+window.onload = function() {
+    $("#start").click(clock.start);
+};
+
+$(function() {
+    $("#main").hide();
+    $("#results").hide();
+    $("#start-button").on("click", function() {
+        $("#start-button, #main").toggle();
+    });
+});
+
+var clock = {
+    time: 10,
+
+    start: function() {
+
+
+        intervalId = setInterval(function() {
+            clock.time--;
+            var converted = clock.timeConverter(clock.time);
+            console.log(converted);
+            $('#time').html(converted);
+            if (clock.time == 0) {
+                $("#results").toggle();
+                $("#main").toggle();
+            };
+        }, 1000);
+    },
+
+    timeConverter: function(t) {
+
+
+        var minutes = Math.floor(t / 60);
+        var seconds = t - (minutes * 60);
+
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        if (minutes === 0) {
+            minutes = "00";
+        } else if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+
+        return minutes + ":" + seconds;
+    },
+
+
+
+};
